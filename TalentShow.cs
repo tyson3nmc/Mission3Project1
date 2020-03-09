794e16709a82eda4c02d7d05863e87234c328a33
tyson3nmc <tyson3@mail.nmc.edu>
2020-03-09 14:20:37 -0400

    Trying to do Happy Birthday...

--- TalentShow.cs
+++ TalentShow.cs
@@ -10,6 +10,18 @@ myFinch.noteOn(250);
 myFinch.wait(500);
 myFinch.noteOff();
 myFinch.wait(100);
+myFinch.noteOn(250);
+myFinch.wait(500);
+myFinch.noteOff();
+myFinch.wait(100);
+myFinch.noteOn(270);
+myFinch.wait(500);
+myFinch.noteOff();
+myFinch.wait(100);
+myFinch.noteOn(250);
+myFinch.wait(500);
+myFinch.noteOff();
+myFinch.wait(100);
 
 myFinch.disConnect();
 }
