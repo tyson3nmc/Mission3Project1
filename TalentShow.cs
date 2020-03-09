f8450eef5ca084c8143d67d73ee64d3ae78ab000
tyson3nmc <tyson3@mail.nmc.edu>
2020-03-09 14:10:14 -0400

    Add some initial notes

--- TalentShow.cs
+++ TalentShow.cs
@@ -6,6 +6,11 @@ static void Main()
 {
 Finch myFinch = new Finch();
 myFinch.connect();
+myFinch.noteOn(250);
+myFinch.wait(500);
+myFinch.noteOff();
+myFinch.wait(100);
+
 myFinch.disConnect();
 }
 }
